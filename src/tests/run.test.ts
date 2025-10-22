2b6bc89a783e6911f4b037f8b49ac38404a4bc97
tshmieldev <wojtekrok88@gmail.com>
2025-10-22 20:38:17 +0200

    more tests

--- src/tests/run.test.ts
+++ src/tests/run.test.ts
@@ -4,28 +4,115 @@ import app from "../controllers/unit-tests";
 import z from "zod";
 import { RunUnitTestRequest } from "../lib/validation";
 
-test("Code grading", async () => {
-    const mockUserCode = await Bun.file(
-        __dirname + "/data/mock-usercode.py",
-    ).text();
-    const mockUnitTests = await Bun.file(
-        __dirname + "/data/mock-unit-tests.py",
-    ).text();
+test("Runalyzer works without errors", async () => {
+  const mockUserCode = await Bun.file(
+    __dirname + "/data/mock-code-1.py",
+  ).text();
+  const mockUnitTests = await Bun.file(
+    __dirname + "/data/mock-tests-1.py",
+  ).text();
 
-    const payload: RunUnitTestRequest = {
-        api_key: process?.env?.API_KEY || "",
-        user_code: mockUserCode,
-        unit_tests: mockUnitTests,
-    };
+  const payload: RunUnitTestRequest = {
+    api_key: process?.env?.API_KEY || "",
+    user_code: mockUserCode,
+    unit_tests: mockUnitTests,
+  };
 
-    const res = await app.request("/", {
-        method: "POST",
-        body: JSON.stringify(payload),
-    });
+  const res = await app.request("/", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
 
-    expect(res.status).toBe(200);
+  expect(res.status).toBe(200);
 
-    const returned = await res.json();
-    console.log(returned);
-    return true;
+  const returned = await res.json();
+
+  return true;
+});
+
+test("Correct code gets max points", async () => {
+  const mockUserCode = await Bun.file(
+    __dirname + "/data/mock-code-1.py",
+  ).text();
+  const mockUnitTests = await Bun.file(
+    __dirname + "/data/mock-tests-1.py",
+  ).text();
+
+  const payload: RunUnitTestRequest = {
+    api_key: process?.env?.API_KEY || "",
+    user_code: mockUserCode,
+    unit_tests: mockUnitTests,
+  };
+
+  const res = await app.request("/", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  expect(res.status).toBe(200);
+
+  const returned = await res.json();
+
+  expect(returned.success).toBe(true);
+  const { total_points, max_points } = returned.runalyzer_output.test_result;
+  expect(total_points).toBe(max_points);
+  return true;
+});
+
+test("Incorrect code gets less than max points", async () => {
+  const mockUserCode = await Bun.file(
+    __dirname + "/data/mock-wrong-1.py",
+  ).text();
+  const mockUnitTests = await Bun.file(
+    __dirname + "/data/mock-tests-1.py",
+  ).text();
+
+  const payload: RunUnitTestRequest = {
+    api_key: process?.env?.API_KEY || "",
+    user_code: mockUserCode,
+    unit_tests: mockUnitTests,
+  };
+
+  const res = await app.request("/", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  expect(res.status).toBe(200);
+
+  const returned = await res.json();
+
+  expect(returned.success).toBe(true);
+  const { total_points, max_points } = returned.runalyzer_output.test_result;
+  expect(total_points).toBe(max_points);
+  return true;
+});
+
+test("Invalid code is handled, shows SyntaxError", async () => {
+  const mockUserCode = await Bun.file(
+    __dirname + "/data/mock-invalid-code.py",
+  ).text();
+  const mockUnitTests = await Bun.file(
+    __dirname + "/data/mock-tests-1.py",
+  ).text();
+
+  const payload: RunUnitTestRequest = {
+    api_key: process?.env?.API_KEY || "",
+    user_code: mockUserCode,
+    unit_tests: mockUnitTests,
+  };
+
+  const res = await app.request("/", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  expect(res.status).toBe(200);
+
+  const returned = await res.json();
+
+  expect(returned.success).toBe(false);
+  expect(returned.runalyzer_errors).toInclude("SyntaxError");
+
+  return true;
 });
