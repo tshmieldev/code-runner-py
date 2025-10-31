0a67f36955bb1665f707f2f54f94b9785a7acbcb
tshmieldev <wojtekrok88@wp.pl>
2025-10-31 21:59:03 +0100

    Fixed type error

--- tests/performance.test.ts
+++ tests/performance.test.ts
@@ -1,46 +1,45 @@
 import { test, expect } from "bun:test";
 import app from "../src/controllers/unit-tests";
 import {
-  runUnitTestResponseSchema,
-  type RunUnitTestRequest,
-  type RunUnitTestResponse,
+    runUnitTestResponseSchema,
+    type RunUnitTestRequest,
+    type RunUnitTestResponse,
 } from "../src/lib/validation";
 
 test("App handles 20 concurrent requests - sorting big arrays", async () => {
-  const mockUserCode = await Bun.file(
-    __dirname + "/data/mock-performance-code.py",
-  ).text();
-  const mockUnitTests = await Bun.file(
-    __dirname + "/data/mock-performance-tests.py",
-  ).text();
+    const mockUserCode = await Bun.file(
+        __dirname + "/data/mock-performance-code.py",
+    ).text();
+    const mockUnitTests = await Bun.file(
+        __dirname + "/data/mock-performance-tests.py",
+    ).text();
 
-  const payload: RunUnitTestRequest = {
-    api_key: process?.env?.API_KEY || "",
-    user_code: mockUserCode,
-    unit_tests: mockUnitTests,
-  };
+    const payload: RunUnitTestRequest = {
+        api_key: process?.env?.API_KEY || "",
+        user_code: mockUserCode,
+        unit_tests: mockUnitTests,
+    };
 
-  const promises = [];
-  for (let i = 0; i < 20; i++) {
-    promises.push(
-      app.request("/", {
-        method: "POST",
-        body: JSON.stringify(payload),
-      }),
-    );
-  }
-  const start = Date.now();
-  const finishedPromises = await Promise.all(promises);
-  const end = Date.now();
-  for (const result of finishedPromises) {
-    expect(result.status).toBe(200);
-    const returned: RunUnitTestResponse = await result.json();
-    runUnitTestResponseSchema.parse(returned);
+    const promises: Promise<Response>[] = [];
+    for (let i = 0; i < 20; i++) {
+        const req = app.request("/", {
+            method: "POST",
+            body: JSON.stringify(payload),
+        });
+        promises.push(req as Promise<Response>);
+    }
+    const start = Date.now();
+    const finishedPromises = await Promise.all(promises);
+    const end = Date.now();
+    for (const result of finishedPromises) {
+        expect(result.status).toBe(200);
+        const returned: RunUnitTestResponse = await result.json();
+        runUnitTestResponseSchema.parse(returned);
 
-    const { total_points, max_total_points } =
-      returned!.runalyzer_output!.test_result;
-    expect(total_points).toBe(max_total_points);
-  }
-  console.log(`Time taken: ${end - start}ms`);
-  expect(end - start).toBeLessThan(5000);
+        const { total_points, max_total_points } =
+            returned!.runalyzer_output!.test_result;
+        expect(total_points).toBe(max_total_points);
+    }
+    console.log(`Time taken: ${end - start}ms`);
+    expect(end - start).toBeLessThan(5000);
 });
